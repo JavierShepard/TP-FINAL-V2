dce384fb5a7be2a32e29ff965eb7a6c6823f4ffb
JavierShepard <comander.javier@gmail.com>
2024-07-15 22:20:05 -0300

    cambios

--- src/config/db.js
+++ src/config/db.js
@@ -6,6 +6,10 @@ const connection = mysql.createConnection({
   password: process.env.MYSQL_ADDON_PASSWORD,
   database: process.env.MYSQL_ADDON_DB,
 });
+console.log('Debug - Database credentials:');
+console.log('Host:', process.env.MYSQL_ADDON_HOST);
+console.log('User:', process.env.MYSQL_ADDON_USER);
+console.log('Database:', process.env.MYSQL_ADDON_DB);
 
 connection.connect((err) => {
   if (err) {
